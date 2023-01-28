94757a161c812ff21b092fac00d381042c63675f
Seulxamor <rlawjdgus850@naver.com>
2023-01-28 20:15:26 +0900

    2023-01-28

---
+++ 김정현/Prac_6/BOJ_2941.java
@@ -0,0 +1,72 @@
+package 김정현.Prac_6;
+
+import java.util.Scanner;
+
+public class BOJ_2941 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        scanner.close();
+
+        int count = 0;
+        for( int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            
+            if(ch == 'c') {
+                if(i < str.length() - 1) {
+                    if (str.charAt(i+1) == '=')
+                        i++;      
+                    else if (str.charAt(i+1) == '-')
+                        i++;  
+                }               
+            }
+
+            else if (ch == 'd') {
+                if(i < str.length() - 1) {
+                    if (str.charAt(i+1) == 'z') {       
+                        if(i < str.length() - 2) {
+                            if (str.charAt(i+2) == '=')
+                                i += 2;
+                        }           
+                    }
+                    else if (str.charAt(i+1) == '-')
+                        i++;
+                }
+                
+            }
+
+            else if (ch == 'l') {
+                if(i < str.length() - 1) {
+                    if (str.charAt(i+1) == 'j')
+                        i++;
+                }
+            }
+
+            else if (ch == 'n') {
+                if(i < str.length() - 1) {
+                    if (str.charAt(i+1) == 'j')
+                        i++;
+                }
+            }
+
+            else if (ch == 's') {
+                if(i < str.length() - 1) {
+                    if (str.charAt(i+1) == '=')
+                        i++;
+                }
+            }
+
+            else if (ch == 'z') {
+                if(i < str.length() - 1) {
+                    if (str.charAt(i+1) == '=')
+                        i++;
+                }
+            }
+            count++;
+        }
+
+        System.out.print(count);
+        
+    }
+}
